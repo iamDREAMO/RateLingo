89cb136bd7cb368bf2c1aef77dd15aca15362824
iamDREAMO <benedictamofah2@gmail.com>
2025-05-29 11:46:47 +0000

    Added CLI flow, user inputs

--- RateLingo.py
+++ RateLingo.py
@@ -11,7 +11,37 @@ def get_service_rate(service):
     return rates.get(service.lower())
 
 
-# Define wordcount
+# Wordcount per-word text
 def count_words(text):
     words = text.strip().split()
     return len(words)
+
+
+# Main Application logic (CLI Flow)
+
+# App's homepage
+def main():
+    print("\nWelcome to RateLingo - The Twi Language Service Pricing Tool\n")
+    print("Available Services: translation, transcription, voiceover, subtitling")
+    
+    # user input: Select Service Type
+    service = input("\nEnter the service type: ").strip().lower()
+    rate = get_service_rate(service)
+    
+    if rate is None:
+        print("‼️ Invalid service. Please choose a valid option.")
+        return
+    
+    # user input: Enter Text
+    print("\nPaste or type the text you want to process. Press Enter twice(2x) to finish:\n")
+    lines = []
+    while True:
+        line = input()
+        if not line:
+            break
+        lines.append(line)
+    
+    
+    
+    
+    
