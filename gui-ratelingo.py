2be6b41ec0d1012fbe9c99080211ea82d433dff5
iamDREAMO <benedictamofah2@gmail.com>
2025-06-11 23:21:43 +0000

    Added a test code to GUI file

---
+++ gui-ratelingo.py
@@ -0,0 +1,9 @@
+# Test sample with Tkinter
+import tkinter
+tkinter._test()
+
+from tkinter import *
+import tkinter as tk
+root = Tk()
+tk.Button(root, text = "Welcome Dreamo").grid()
+root.mainloop()
