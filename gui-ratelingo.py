ffc3ef06add96dbcb7b89377986272d12e676a24
iamDREAMO <benedictamofah2@gmail.com>
2025-06-30 17:57:15 +0000

    GUI: Basic GUI program

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -1,5 +1,12 @@
 # Test sample with Tkinter
-import tkinter
-tkinter._test()
+#import tkinter
+#tkinter._test()
 
+# Building Basic GUI Program
+from tkinter import *
+
+myroot = Tk()     #creating an object of Tk class
+myroot.resizable(width = True, height = False) #maximize the width
+
+myroot.mainloop()
 
