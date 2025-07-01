a39ff83c10270cbcb07efd43767dfb85e092e1f7
iamDREAMO <benedictamofah2@gmail.com>
2025-07-01 16:19:01 +0000

    GUI: Added wraplength, height, underline, width

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -8,7 +8,6 @@ from tkinter import *
 # Resizing/Class
 myroot = Tk()   # Creating an object of Tk class
 myroot.resizable(width = True, height = True)   # Resize option for width/height 
-
 myroot.mainloop()
 
 
@@ -18,17 +17,34 @@ from tkinter import *
 myroot = Tk() 
 myl1 = Label(myroot, text = 'Label1', bd = 5, relief = 'groove')   # Lable feature
 myl1.pack()
-
 myroot.mainloop()
+
 
 # Highlighttickness
 from tkinter import *
 
 myroot = Tk() 
-myb1 = Button(myroot, text = 'Without HT')   # Hihlighttickness feature
+myb1 = Button(myroot, text = 'Without HT')   # No Hihlighttickness feature
 myb1.grid(row = 0, column = 1)
 
-myb2 = Button(myroot, text = 'With HT', highlightthickness=10)
-myb2.grid(row=1, column=1, padx=10, pady=10)
+myb2 = Button(myroot, text = 'With HT', highlightthickness=10)   # Hihlighttickness feature
+myb2.grid(row=1, column=1, padx=10, pady=10)   # With padX and padY
+myroot.mainloop()
 
+
+# Wraplength
+from tkinter import *
+myroot = Tk()
+myl1 = Label(myroot, text = 'KOFI', wraplength=2)   # with wrplength
+myl1.pack()
+myl2 = Label(myroot, text = 'AMAZING', wraplength=0)
+myl2.pack()
+myroot.mainloop()
+
+
+# Height, Underline, Width
+from tkinter import *
+myroot = Tk()
+myl1 = Label(myroot, text = 'BENEDICT', width=15, height=4, underline=3, font=('Cambria', 14))
+myl1.pack()
 myroot.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,5 @@
 from tkinter import *
-
-myroot = Tk() 
-myb1 = Button(myroot, text = 'Without HT')   # Hihlighttickness feature
-myb1.grid(row = 0, column = 1)
-
-myb2 = Button(myroot, text = 'With HT', highlightthickness=10)
-myb2.grid(row=1, column=1, padx=10, pady=10)
-
+myroot = Tk()
+myl1 = Label(myroot, text = 'BENEDICT', width=15, height=4, underline=3, font=('Cambria', 14))
+myl1.pack()
 myroot.mainloop()
