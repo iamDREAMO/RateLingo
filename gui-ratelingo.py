2c77f764bbaeb3246b0a8854d8d1297615dcd63b
iamDREAMO <benedictamofah2@gmail.com>
2025-07-02 18:07:42 +0000

    GUI: Added Relief Styles

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -3,7 +3,7 @@
 #tkinter._test()
 
 # Building Basic GUI Program
-from tkinter import *
+from tkinter import * # type: ignore
 
 # Resizing/Class
 myroot = Tk()   # Creating an object of Tk class
@@ -15,7 +15,7 @@ myroot.mainloop()
 
 # DIMENSIONS:
 # Borderwidth
-from tkinter import *
+from tkinter import * # type: ignore
 
 myroot = Tk() 
 myl1 = Label(myroot, text = 'Label1', bd = 5, relief = 'groove')   # borderwidth feature
@@ -24,7 +24,7 @@ myroot.mainloop()
 
 
 # Highlighttickness, padX, padY
-from tkinter import *
+from tkinter import * # type: ignore
 
 myroot = Tk() 
 myb1 = Button(myroot, text = 'Without HT')   # No Hihlighttickness feature
@@ -36,7 +36,7 @@ myroot.mainloop()
 
 
 # Wraplength
-from tkinter import *
+from tkinter import * # type: ignore
 myroot = Tk()
 myl1 = Label(myroot, text = 'KOFI', wraplength=2)   # with wrplength
 myl1.pack()
@@ -46,7 +46,7 @@ myroot.mainloop()
 
 
 # Height, Underline, Width
-from tkinter import *
+from tkinter import * # type: ignore
 myroot = Tk()
 myl1 = Label(myroot, text = 'BENEDICT', width=15, height=4, underline=3, font=('Cambria', 14))
 myl1.pack()
@@ -55,7 +55,7 @@ myroot.mainloop()
 
 # COLORS:
 # Activebackground, background
-from tkinter import *
+from tkinter import * # type: ignore
 myroot = Tk()
 myb1 = Button(myroot, activebackground= "Magenta2", bg = '#FFC000', text= 'DREAMO')
 myb1.pack()
@@ -63,7 +63,7 @@ myroot.mainloop()
 
 
 # Activeforeground, foreground
-from tkinter import *
+from tkinter import * # type: ignore
 myroot = Tk()
 myb1 = Button(myroot, activeforeground= 'Cyan2', fg= 'Salmon', text= 'GHANA')
 myb1.pack()
@@ -71,7 +71,7 @@ myroot.mainloop()
 
 
 # Disabledforeground
-from tkinter import *
+from tkinter import * # type: ignore
 myroot = Tk()
 myb1 = Button(myroot, state = 'disabled', text = 'BONO', disabledforeground= 'red')
 myb1.pack()
@@ -79,7 +79,7 @@ myroot.mainloop()
 
 
 # Highlightbackground
-from tkinter import *
+from tkinter import * # type: ignore
 myroot = Tk()
 mye1 = Entry(myroot, bg= 'Cyan3', highlightthickness=10, highlightbackground= 'Slateblue1', highlightcolor= 'Black')
 mye1.pack(padx= 5, pady=5)
@@ -90,7 +90,7 @@ myroot.mainloop()
 
 
 # Selectbackground, selectforeground
-from tkinter import *
+from tkinter import * # type: ignore
 myroot = Tk()
 str1 = StringVar()
 mye1 = Entry(myroot, selectbackground='OliveDrab1', selectforeground= 'Deep Pink', textvariable= str1)
@@ -100,11 +100,11 @@ myroot.mainloop()
 
 
 # FONTS
-from tkinter import *
+from tkinter import * #type: ignore
 from tkinter.font import Font
 myroot = Tk()
 
-myfont1 = Font(family= 'Helvetica', size= 14, weight='bold', underline= 0, slant = 'roman', overstrike= 0)
+myfont1 = Font(family= 'Helvetica', size= 14, weight='bold', underline= 0, slant = 'roman', overstrike= 0) # type: ignore
 myl1= Label(myroot, text = 'BENEDICT', font = myfont1)
 myl1.pack() # for displaying the label
 
@@ -112,7 +112,7 @@ myroot.mainloop() # display the window until we press the close button (x)
  
 
 # Viewing the Font family
-from tkinter import * # Importing module
+from tkinter import * # type: ignore # Importing module
 from tkinter import font
 myroot = Tk() # window creation and initialize the interpreter
 
@@ -123,21 +123,39 @@ for loop in myfont_list:
 myroot.mainloop()
 
 # Using tuple to access the font family
-from tkinter import *
+from tkinter import * # type: ignore
 from tkinter.font import Font
 myroot = Tk()
 
-myl1 = Label(myroot, text = 'DREAMO', font = ('Arial', '18', 'bold roman underline'))
+myl1 = Label(myroot, text = 'DREAMO', font = ('Arial', '18', 'bold roman underline')) # type: ignore
 myl1.pack()
 myroot.mainloop()
 
 
 # ANCHORS (N.S,E,W,NE,NW,SE,SW,CENTER)
-from tkinter import *
+from tkinter import * # type: ignore
 myroot = Tk()
 myroot.geometry('200x200')
 
 # Choose anchor and code remains the same
-myl1 = Label(myroot, text= 'KOTEI', anchor= CENTER, font =('Arila', '20', 'bold roman'), bd = 2, relief = 'raised', width = 10, height = 6)
+myl1 = Label(myroot, text= 'KOTEI', anchor= CENTER, font =('Arial', '20', 'bold roman'), bd = 2, relief = 'raised', width = 10, height = 6) # type: ignore
 myl1.pack()
+myroot.mainloop()
+
+
+# RELIEF STYLES
+from tkinter import * # type: ignore
+
+myroot = Tk()
+myroot.geometry('300x200')
+myb1 = Button(myroot, text= 'DREAMO', font = ('Roboto', 12), relief= FLAT, bd =5)
+myb1.pack()
+myb2 = Button(myroot, text= 'DREAMO', font = ('Roboto', 12), relief= GROOVE, bd =5)
+myb2.pack()
+myb3 = Button(myroot, text= 'DREAMO', font = ('Roboto', 12), relief= SUNKEN, bd =5)
+myb3.pack()
+myb4 = Button(myroot, text= 'DREAMO', font = ('Roboto', 12), relief= RAISED, bd =5)
+myb4.pack()
+myb5 = Button(myroot, text= 'DREAMO', font = ('Roboto', 12), relief= RIDGE, bd =5)
+myb5.pack()
 myroot.mainloop()
