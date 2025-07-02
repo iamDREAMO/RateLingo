9d85ab03ead7aee8f9afafd984e940415c7eb4ac
iamDREAMO <benedictamofah2@gmail.com>
2025-07-02 10:42:34 +0000

    GUI: Added selectbackgrounds

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,7 @@
 from tkinter import *
 myroot = Tk()
-mye1 = Entry(myroot, bg= 'Cyan3', highlightthickness=10, highlightbackground= 'Slateblue1', highlightcolor= 'Black')
-mye1.pack(padx= 5, pady=5)
-mye2 = Entry(myroot)
-mye2.pack()
-mye2.focus()
+str1 = StringVar()
+mye1 = Entry(myroot, selectbackground='OliveDrab1', selectforeground= 'Deep Pink', textvariable= str1)
+mye1.pack()
+str1.set('TREDE')
 myroot.mainloop()
