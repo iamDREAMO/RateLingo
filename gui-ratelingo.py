7fb055a4cf786978259d202491bb9cd4bc46746c
iamDREAMO <benedictamofah2@gmail.com>
2025-07-02 16:29:46 +0000

    GUI: Added Fonts

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -98,3 +98,15 @@ mye1.pack()
 str1.set('TREDE')
 myroot.mainloop()
 
+
+# FONTS
+from tkinter import *
+from tkinter.font import Font
+myroot = Tk()
+
+myfont1 = Font(family= 'Helvetica', size= 14, weight='bold', underline= 0, slant = 'roman', overstrike= 0)
+myl1= Label(myroot, text = 'BENEDICT', font = myfont1)
+myl1.pack()
+
+myroot.mainloop()
+
