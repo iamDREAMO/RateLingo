2e3bf3a3b5b3b7ff1ee863d70ed5374455ef57ed
iamDREAMO <benedictamofah2@gmail.com>
2025-07-02 17:24:59 +0000

    GUI: Added anchors

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,8 @@
 from tkinter import *
-from tkinter.font import Font
 myroot = Tk()
+myroot.geometry('200x200')
 
-myl1 = Label(myroot, text = 'DREAMO', font = ('Arial', '18', 'bold roman underline'))
+# Choose anchor and code remains the same
+myl1 = Label(myroot, text= 'KOTEI', anchor= CENTER, font =('Arila', '20', 'bold roman'), bd = 2, relief = 'raised', width = 10, height = 6)
 myl1.pack()
 myroot.mainloop()
