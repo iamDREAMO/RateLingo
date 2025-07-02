ee91432cfae0621fc73c9ec7f656ce47f94167c2
iamDREAMO <benedictamofah2@gmail.com>
2025-07-02 17:01:40 +0000

    GUI: Used tuple method to access font

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from tkinter.font import Font
+myroot = Tk()
+
+myl1 = Label(myroot, text = 'DREAMO', font = ('Arial', '18', 'bold roman underline'))
+myl1.pack()
+myroot.mainloop()
