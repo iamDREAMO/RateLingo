e4596bbb00f863bff8e5951a78349e900d7f0412
iamDREAMO <benedictamofah2@gmail.com>
2025-07-02 17:24:59 +0000

    GUI: Added anchors

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -130,3 +130,14 @@ myroot = Tk()
 myl1 = Label(myroot, text = 'DREAMO', font = ('Arial', '18', 'bold roman underline'))
 myl1.pack()
 myroot.mainloop()
+
+
+# ANCHORS (N.S,E,W,NE,NW,SE,SW,CENTER)
+from tkinter import *
+myroot = Tk()
+myroot.geometry('200x200')
+
+# Choose anchor and code remains the same
+myl1 = Label(myroot, text= 'KOTEI', anchor= CENTER, font =('Arila', '20', 'bold roman'), bd = 2, relief = 'raised', width = 10, height = 6)
+myl1.pack()
+myroot.mainloop()
