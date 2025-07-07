742db22851290cac06279de484b9ddbeb440b278
iamDREAMO <benedictamofah2@gmail.com>
2025-07-07 13:57:29 +0000

    GUI: Added position change

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 myroot = Tk()   # Creating blank tkinter widow
-myroot.geometry('300x150')
+myroot.geometry('300x300+500+400')
 
 mybtn = Button(myroot, text= 'OWUSU')
 mybtn.pack(side= TOP, padx = 5, pady = 5)
