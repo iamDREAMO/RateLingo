136565a3af384e84d13a75d85d6ce049e22ed909
iamDREAMO <benedictamofah2@gmail.com>
2025-07-07 13:57:29 +0000

    GUI: Added position change

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -289,3 +289,13 @@ myroot.geometry('300x150')
 mybtn = Button(myroot, text= 'OWUSU')
 mybtn.pack(side= TOP, padx = 5, pady = 5)
 myroot.mainloop()
+
+# Changing the position of the window
+from tkinter import *
+
+myroot = Tk()   # Creating blank tkinter widow
+myroot.geometry('300x150+400+400')
+
+mybtn = Button(myroot, text= 'OWUSU')
+mybtn.pack(side= TOP, padx = 5, pady = 5)
+myroot.mainloop()
