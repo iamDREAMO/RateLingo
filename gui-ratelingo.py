f664626187ab6e9b2dc70bc301846fe54ca38b09
iamDREAMO <benedictamofah2@gmail.com>
2025-07-08 11:11:19 +0000

    GUI: Added BooleanVar()

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -324,3 +324,27 @@ my_btn = Button(myroot, font = ('Cambria', 13), text= 'Get Data', command= mysho
 my_btn.pack()
 
 myroot.mainloop()
+
+
+# BooleanVar()
+from tkinter import *
+
+myroot = Tk()
+myroot.geometry('300x200')
+
+num1 = BooleanVar()
+mystr = StringVar()
+
+def mydatainsertion():
+    if num1.get() == True:
+        mystr.set('It is set to True')
+    else:
+        mystr.set('It is set to False')
+        
+myc1 = Checkbutton(myroot, variable= num1, font = ('Cambria', 13), text= 'DREAMO', command= mydatainsertion)
+myc1.pack()
+
+mye1 = Entry(myroot, width= 20, textvariable= mystr)
+mye1.pack()
+
+myroot.mainloop() 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,20 +1,21 @@
 from tkinter import *
-myroot = Tk()   # creating an oblject of Tk class -- object of window
 
-myroot.geometry('200x200') # window can be resized to any size until we use root.resizeable
-myroot.resizable(0,0)   # window size is fixed (cannot be larger/smaller)
+myroot = Tk()
+myroot.geometry('300x200')
 
-mystr = StringVar() # S1
-print(type(mystr))  # S2
-my_entry = Entry(myroot, font = ('Cambria, 13'), textvariable = mystr)
-my_entry.pack()
+num1 = BooleanVar()
+mystr = StringVar()
 
-def myshow():
-    mydata = mystr.get() #S3
-    print(mydata)
-    mystr.set('') # S4
-    
-my_btn = Button(myroot, font = ('Cambria', 13), text= 'Get Data', command= myshow)
-my_btn.pack()
+def mydatainsertion():
+    if num1.get() == True:
+        mystr.set('It is set to True')
+    else:
+        mystr.set('It is set to False')
+        
+myc1 = Checkbutton(myroot, variable= num1, font = ('Cambria', 13), text= 'DREAMO', command= mydatainsertion)
+myc1.pack()
 
-myroot.mainloop()
+mye1 = Entry(myroot, width= 20, textvariable= mystr)
+mye1.pack()
+
+myroot.mainloop() 
