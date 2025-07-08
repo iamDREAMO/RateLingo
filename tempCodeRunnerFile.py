1da593e63208580243e7ff18de1382779791a975
iamDREAMO <benedictamofah2@gmail.com>
2025-07-08 11:35:26 +0000

    GUI: Added IntVar()

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,21 +1,29 @@
 from tkinter import *
-
 myroot = Tk()
-myroot.geometry('300x200')
+myroot.geometry('300x300')
+myroot.resizable(0,0)
 
-num1 = BooleanVar()
-mystr = StringVar()
+myint = IntVar()
+myint1 = IntVar()
+myint2 = IntVar()
 
-def mydatainsertion():
-    if num1.get() == True:
-        mystr.set('It is set to True')
-    else:
-        mystr.set('It is set to False')
-        
-myc1 = Checkbutton(myroot, variable= num1, font = ('Cambria', 13), text= 'DREAMO', command= mydatainsertion)
-myc1.pack()
+my_entry = Entry(myroot, font = ('Cambria', 13), textvariable= myint)
+my_entry.pack()
 
-mye1 = Entry(myroot, width= 20, textvariable= mystr)
-mye1.pack()
+my_entry1 = Entry(myroot, font = ('Cambria', 13), textvariable= myint1)
+my_entry1.pack()
 
-myroot.mainloop() 
+
+def mydisplay():
+    mydata1 = myint.get()
+    mydata2 = myint1.get()
+    mydata3 = mydata1 * mydata2
+    myint2.set(mydata3)
+    
+my_btn = Button(myroot, font = ('Cambria', 13,), text= 'Multiply', command= mydisplay)
+my_btn.pack()
+my_entry2 = Entry(myroot, font= ('Cambria', 12), textvariable= myint2)
+my_entry2.pack()
+my_entry2.configure(state = 'readonly')
+
+myroot.mainloop()
