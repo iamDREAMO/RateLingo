733f59f75fd6be47d64f9479c60ecbbdb3bb7436
iamDREAMO <benedictamofah2@gmail.com>
2025-07-08 10:47:32 +0000

    GUI: Added StringVar()

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,20 @@
 from tkinter import *
+myroot = Tk()   # creating an oblject of Tk class -- object of window
 
-myroot = Tk()   # Creating blank tkinter widow
-myroot.geometry('300x300+500+400')
+myroot.geometry('200x200') # window can be resized to any size until we use root.resizeable
+myroot.resizable(0,0)   # window size is fixed (cannot be larger/smaller)
 
-mybtn = Button(myroot, text= 'OWUSU')
-mybtn.pack(side= TOP, padx = 5, pady = 5)
+mystr = StringVar() # S1
+print(type(mystr))  # S2
+my_entry = Entry(myroot, font = ('Cambria, 13'), textvariable = mystr)
+my_entry.pack()
+
+def myshow():
+    mydata = mystr.get() #S3
+    print(mydata)
+    mystr.set('') # S4
+    
+my_btn = Button(myroot, font = ('Cambria', 13), text= 'Get Data', command= myshow)
+my_btn.pack()
+
 myroot.mainloop()
