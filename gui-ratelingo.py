4ce2b073d802382f5e0d5d15cceb0ad707d67e7b
iamDREAMO <benedictamofah2@gmail.com>
2025-07-08 10:47:32 +0000

    GUI: Added StringVar()

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -299,3 +299,28 @@ myroot.geometry('300x150+400+400')
 mybtn = Button(myroot, text= 'OWUSU')
 mybtn.pack(side= TOP, padx = 5, pady = 5)
 myroot.mainloop()
+
+
+# Inbuilt Variable Classes
+
+# SringVar()
+from tkinter import *
+myroot = Tk()   # creating an oblject of Tk class -- object of window
+
+myroot.geometry('200x200') # window can be resized to any size until we use root.resizeable
+myroot.resizable(0,0)   # window size is fixed (cannot be larger/smaller)
+
+mystr = StringVar() # S1
+print(type(mystr))  # S2
+my_entry = Entry(myroot, font = ('Cambria, 13'), textvariable = mystr)
+my_entry.pack()
+
+def myshow():
+    mydata = mystr.get() #S3
+    print(mydata)
+    mystr.set('') # S4
+    
+my_btn = Button(myroot, font = ('Cambria', 13), text= 'Get Data', command= myshow)
+my_btn.pack()
+
+myroot.mainloop()
