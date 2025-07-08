e7343c9d2978f682d4816b9c7df9c690d10385c9
iamDREAMO <benedictamofah2@gmail.com>
2025-07-08 11:35:26 +0000

    GUI: Added IntVar()

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -348,3 +348,35 @@ mye1 = Entry(myroot, width= 20, textvariable= mystr)
 mye1.pack()
 
 myroot.mainloop() 
+
+
+# IntVar()
+from tkinter import *
+myroot = Tk()
+myroot.geometry('300x300')
+myroot.resizable(0,0)
+
+myint = IntVar()
+myint1 = IntVar()
+myint2 = IntVar()
+
+my_entry = Entry(myroot, font = ('Cambria', 13), textvariable= myint)
+my_entry.pack()
+
+my_entry1 = Entry(myroot, font = ('Cambria', 13), textvariable= myint1)
+my_entry1.pack()
+
+
+def mydisplay():
+    mydata1 = myint.get()
+    mydata2 = myint1.get()
+    mydata3 = mydata1 * mydata2
+    myint2.set(mydata3)
+    
+my_btn = Button(myroot, font = ('Cambria', 13, 'bold',), text= 'Multiply', command= mydisplay)
+my_btn.pack()
+my_entry2 = Entry(myroot, font= ('Cambria', 12), textvariable= myint2)
+my_entry2.pack()
+my_entry2.configure(state = 'readonly')
+
+myroot.mainloop()
