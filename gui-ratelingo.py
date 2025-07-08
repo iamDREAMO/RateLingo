1a22753c81607593150e28145775e2c140d11f39
iamDREAMO <benedictamofah2@gmail.com>
2025-07-08 12:59:29 +0000

    GUI: Added OOP techniques for widgets integration

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -427,3 +427,19 @@ mybtn = Button(myroot, text= 'Display', font= ('Cambria', 13, 'bold',), command=
 mybtn.pack(padx= 20, pady= 30)
 myroot.mainloop()
 
+# Using OOP techniques to address more widgets integration
+from tkinter import *
+from tkinter import messagebox
+
+class MyBtn(Tk):
+    def __init__(self): # constructor
+        super().__init__() # for calling the constructor of superclass
+        self.mybtn = Button(self, text= 'Display', command= self.mydisplay)
+        self.mybtn.pack(padx= 20, pady= 30)
+        
+    def mydisplay(self):
+        messagebox.showinfo('Message', 'Hey Kofi!')
+        
+if __name__ == '__main__':
+    myroot = MyBtn() # making an object of MyBtn class
+    myroot.mainloop()
