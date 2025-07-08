6c7b28aa301d1e71a18376c10ac1d3e7a98c7939
iamDREAMO <benedictamofah2@gmail.com>
2025-07-08 12:08:32 +0000

    GUI: Added messagebox

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,30 +1,10 @@
 from tkinter import *
+from tkinter import messagebox
 myroot = Tk()
-myroot.geometry('300x300')
-myroot.resizable(0,0)
-
-myint = DoubleVar()
-myint1 = DoubleVar()
-myint2 = DoubleVar()
-
-my_entry = Entry(myroot, font= ('Cambria', 13,'bold',), textvariable= myint)
-my_entry.pack()
-
-my_entry1 = Entry(myroot, font= ('Cambria', 13, 'bold',), textvariable=myint1)
-my_entry1.pack()
-
 
 def mydisplay():
-    mydata1 = myint.get()
-    mydata2 = myint1.get()
-    mydata3 = mydata1 - mydata2
-    myint2.set(mydata3)
+    messagebox.showinfo('Message', 'Hey Kofi!')
     
-my_btn = Button(myroot, font= ('Cambria', 13, 'bold',), text= 'Difference', command= mydisplay)
-my_btn.pack()
-
-my_entry2 = Entry(myroot, font= ('Cambria', 13), textvariable= myint2)
-my_entry2.pack()
-my_entry2.configure(state= 'readonly')
-
+mybtn = Button(myroot, text= 'Display', font= ('Cambria', 13, 'bold',), command= mydisplay)
+mybtn.pack(padx= 20, pady= 30)
 myroot.mainloop()
