e6d167dc77c402f727afbe3dbf3f2bcd5b628458
iamDREAMO <benedictamofah2@gmail.com>
2025-07-09 05:21:00 +0000

    GUI: Added image- tk button

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -479,3 +479,24 @@ if __name__ == "__main__":
     myroot = MyJustify()
     myroot.geometry('350x350')
     myroot.mainloop()
+    
+
+# Image and State- button insights
+from tkinter import *
+
+class MyBtnImage(Frame):
+    def __init__(self, root = None):
+        Frame.__init__(self, root)
+        self.root = root
+        self.myphoto = PhotoImage(file = 'Add-icon.png')
+        def myclick():
+            self.mybtn1['state'] = DISABLED
+        self.mybtn1 = Button(self.root, image = self.myphoto, height= 200, command= myclick)
+        self.mybtn1.pack(padx = 10, pady = 10)
+        
+if __name__ == '__main__':
+    myroot = Tk()
+    myobj = MyBtnImage(myroot)
+    myroot.title('Image using Button')
+    myroot.geometry('300x300')
+    myroot.mainloop()
