b27900c59448deb420f650898bf24524bbe52ae4
iamDREAMO <benedictamofah2@gmail.com>
2025-07-09 05:21:00 +0000

    GUI: Added image- tk button

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,31 +1,18 @@
 from tkinter import *
-from tkinter import messagebox
 
-class MyJustify(Tk):
-    def __init__(self):
-        super(). __init__()
-        self.title('Justify in Button')
+class MyBtnImage(Frame):
+    def __init__(self, root = None):
+        Frame.__init__(self, root)
+        self.root = root
+        self.myphoto = PhotoImage(file = 'Add-icon.png')
+        def myclick():
+            self.mybtn1['state'] = DISABLED
+        self.mybtn1 = Button(self.root, image = self.myphoto, height= 200, command= myclick)
+        self.mybtn1.pack(padx = 10, pady = 10)
         
-        def mycenterjustify():
-            messagebox.showinfo('Justify', 'Justify CENTER')
-            
-        def myleftjustify():
-            messagebox.showinfo('Justify', 'Justify LEFT')
-            
-        def myrightjustify():
-            messagebox.showinfo('Justify', 'Justify RIGHT')
-            
-        # Default justify in CENTER
-        mybtn1 = Button(self, text= 'JUSTIFY\nCENTER\nCENTER CENTER', bd= 3, relief = 'groove', font= ('Cambria', 13), width = 20, height = 3, command = mycenterjustify)
-        mybtn1.pack(pady= 10, side = BOTTOM)
-        
-        mybtn2 = Button(self, text= 'JUSTIFY\nLEFT\nLEFT LEFT', bd= 3, relief = 'groove', font= ('Cambria', 13), justify= LEFT, width = 20, height = 3, command = myleftjustify)
-        mybtn2.pack(pady= 10, side = RIGHT)
-        
-        mybtn3 = Button(self, text= 'JUSTIFY\nRIGHT\nRIGHT RIGHT', bd= 2, font= ('Cambria', 13), justify= RIGHT, width = 20, height = 3, command = myrightjustify)
-        mybtn3.pack(side = TOP)
-        
-if __name__ == "__main__":
-    myroot = MyJustify()
-    myroot.geometry('350x350')
+if __name__ == '__main__':
+    myroot = Tk()
+    myobj = MyBtnImage(myroot)
+    myroot.title('Image using Button')
+    myroot.geometry('300x300')
     myroot.mainloop()
