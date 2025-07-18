3460c5214aa5bce2a99714a9e644a45c71bd264e
iamDREAMO <benedictamofah2@gmail.com>
2025-07-18 12:07:01 +0000

    GUI: Add a single function

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -640,3 +640,26 @@ myroot.bind('<Key-b>', lambda e: myroot.configure(background='LightGreen')) # on
 myroot.bind('<Key-c>', lambda e: myroot.configure(background='Pink')) # on pressing key 'c'
 
 myroot.mainloop()
+
+
+# Event handling using single function
+from tkinter import *
+myroot = Tk()
+myroot.geometry('200x200')
+myroot.resizable(0,0)
+
+num = 1
+def mydisplay(e):
+    global num
+    num = num + 1
+    if num%2 == 0:
+        myroot.configure(background='LightBlue')
+    else:
+        myroot.configure(background='Brown')
+        
+mybtn1 = Button(myroot, text= 'Click Here!!!', font=('Cambria', 15, 'bold'))
+mybtn1.bind('<Button>', mydisplay) # on mouse pressing button
+mybtn1.bind('<ButtonRelease>', mydisplay) # on mouse releasing button
+mybtn1.pack()
+
+myroot.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,20 @@
 from tkinter import *
 myroot = Tk()
-myroot.geometry('300x300')
+myroot.geometry('200x200')
 myroot.resizable(0,0)
 
-myroot.bind('<Key-a>', lambda e: myroot.configure(background='LightBlue')) # on pressing key 'a'
-myroot.bind('<Key-b>', lambda e: myroot.configure(background='LightGreen')) # on pressing key 'b'
-myroot.bind('<Key-c>', lambda e: myroot.configure(background='Pink')) # on pressing key 'c'
+num = 1
+def mydisplay(e):
+    global num
+    num = num + 1
+    if num%2 == 0:
+        myroot.configure(background='LightBlue')
+    else:
+        myroot.configure(background='Brown')
+        
+mybtn1 = Button(myroot, text= 'Click Here!!!', font=('Cambria', 15, 'bold'))
+mybtn1.bind('<Button>', mydisplay) # on mouse pressing button
+mybtn1.bind('<ButtonRelease>', mydisplay) # on mouse releasing button
+mybtn1.pack()
 
 myroot.mainloop()
