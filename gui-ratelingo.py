04dd84ef2684f3e71562d91b60e5d0cbd834c61c
iamDREAMO <benedictamofah2@gmail.com>
2025-07-18 11:57:54 +0000

    GUI: Add event handling alphabet keys

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -627,3 +627,16 @@ mytk_btn1.bind('<Button-3>', myshow3) # right key of mouse
 mytk_btn1.pack()
 
 myroot.mainloop()
+
+
+# Event handling using Alphabet Keys
+from tkinter import *
+myroot = Tk()
+myroot.geometry('300x300')
+myroot.resizable(0,0)
+
+myroot.bind('<Key-a>', lambda e: myroot.configure(background='LightBlue')) # on pressing key 'a'
+myroot.bind('<Key-b>', lambda e: myroot.configure(background='LightGreen')) # on pressing key 'b'
+myroot.bind('<Key-c>', lambda e: myroot.configure(background='Pink')) # on pressing key 'c'
+
+myroot.mainloop()
