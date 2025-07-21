6acd0b2d37b2b59039386067f7ca44b35627a266
iamDREAMO <benedictamofah2@gmail.com>
2025-07-21 12:06:20 +0000

    GUI: Add Checkbutton widget

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -663,3 +663,28 @@ mybtn1.bind('<ButtonRelease>', mydisplay) # on mouse releasing button
 mybtn1.pack()
 
 myroot.mainloop()
+
+
+# tkinter Checkbutton Widget
+from tkinter import *
+from tkinter.ttk import *
+
+myroot =Tk()
+myroot.geometry('200x200')
+myroot.title('CheckButton Widget')
+
+def myget():
+    if i2.get() == 'Check':
+        s1.set('Checked')
+    else:
+        s1.set('Unchecked')
+        
+i2 = StringVar()
+myc2 = Checkbutton(myroot, text= 'Check/Uncheck', variable = i2, offvalue= 'Uncheck', onvalue= 'Check', command= myget)
+myc2.pack()
+
+s1 = StringVar()
+mye1 = Entry(myroot, font= ('Cambria', 14, 'bold'), textvariable= s1)
+mye1.pack(pady=10)
+
+myroot.mainloop()
