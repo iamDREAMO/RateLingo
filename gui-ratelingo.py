4aeffacaf449924970a6d461f82fc880cdcd8627
iamDREAMO <benedictamofah2@gmail.com>
2025-07-21 15:08:54 +0000

    GUI: Add text to Radiobutton

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -822,3 +822,14 @@ myrb1.pack(padx=10, pady=10)
 myrb2.pack(padx=10, pady=10)
 
 myroot.mainloop()
+
+# Radiobutton with text
+from tkinter import *
+myroot = Tk()
+
+myrb1 = Radiobutton(myroot, value=0, text='Radiobutton1', bg= 'lightGreen', indicatoron=False)
+myrb2 = Radiobutton(myroot, value=1, text='RadioButton2', bg = 'lightGreen', indicatoron=False)
+myrb1.pack(padx=10,pady=10)
+myrb2.pack(padx=10,pady=10)
+
+myroot.mainloop()
