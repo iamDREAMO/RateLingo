a41a02e3428a8f2875d0529d6601e9a970798a32
iamDREAMO <benedictamofah2@gmail.com>
2025-07-21 12:50:58 +0000

    GUI: Add 'selectimage()' option

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,16 @@
 from tkinter import *
-myroot= Tk()
-def selectcolor_indicatoronTrue():
-    mychk1['selectcolor'] = 'Green'
-    
-def selectcolor_indicatoronFalse():
-    mychk2['selectcolor'] = 'Yellow'
-    
-mychk1 = Checkbutton(myroot, text= 'CheckButton', command=selectcolor_indicatoronTrue, indicatoron=True)
-mychk1.place(x=50, y=50)
-mychk2 = Checkbutton(myroot, text= 'CheckButton', command=selectcolor_indicatoronFalse, indicatoron=False)
-mychk2.place(x=50, y=100)
+myroot =Tk()
+
+myon_image = PhotoImage(width=50, height=25)
+myoff_image = PhotoImage(width=50, height=25)
+myon_image.put('LightGreen', to=(0, 0,24, 24)) # put row formatted colors to image starting from position TO
+myoff_image.put(('Red',), to=(25, 0 , 49, 24))
+
+myval1 = IntVar(value=0)
+myval2 = IntVar(value=1)
+cb1 = Checkbutton(myroot, image= myoff_image, selectimage=myon_image, indicatoron=False, onvalue=1, offvalue=0, variable=myval1)
+cb2 = Checkbutton(myroot, image=myoff_image, selectimage=myon_image, indicatoron=False, onvalue=1, offvalue=0, variable=myval2)
+cb1.pack(padx=10, pady=10)
+cb2.pack(padx=10, pady=10)
+
 myroot.mainloop()
