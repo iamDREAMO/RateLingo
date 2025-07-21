9085a213614dc3c0231eec6ba434e87271cca89d
iamDREAMO <benedictamofah2@gmail.com>
2025-07-21 15:08:54 +0000

    GUI: Add text to Radiobutton

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,15 +1,9 @@
 from tkinter import *
 myroot = Tk()
 
-myon_image = PhotoImage(width=50, height=25)
-myoff_image = PhotoImage(width=50, height=25)
-myon_image.put(('Brown'), to=(0, 0 , 24, 24)) # put row formatted colors to image starting from position TO
-myoff_image.put(('Red',), to=(0,0,24,24))
-
-myrbvar = IntVar(value=1)
-myrb1 = Radiobutton(myroot, variable=myrbvar, value=0, bd=0, text='RadioButton1', compound='left', indicatoron=False, image=myoff_image, selectimage=myon_image)
-myrb2 = Radiobutton(myroot, variable=myrbvar, value=1, bd=0, text='Radiobutton2', compound='left', indicatoron=False, image=myoff_image, selectimage=myon_image)
-myrb1.pack(padx=10, pady=10)
-myrb2.pack(padx=10, pady=10)
+myrb1 = Radiobutton(myroot, value=0, text='Radiobutton1', bg= 'lightGreen', indicatoron=False)
+myrb2 = Radiobutton(myroot, value=1, text='RadioButton2', bg = 'lightGreen', indicatoron=False)
+myrb1.pack(padx=10,pady=10)
+myrb2.pack(padx=10,pady=10)
 
 myroot.mainloop()
