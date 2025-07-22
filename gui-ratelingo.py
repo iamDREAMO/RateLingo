717b3e463cf8ec264eb2639a80e6a7b59f4e2c77
iamDREAMO <benedictamofah2@gmail.com>
2025-07-22 13:11:55 +0000

    GUI: Add state/textvarible to Entry()

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -856,3 +856,36 @@ mybtn = Button(myroot, text= 'My Choice',command=mydisplay)
 mybtn.pack(pady=30, side= BOTTOM, anchor= N)
 
 myroot.mainloop()
+
+
+# Insight of the Input Widget in tkinter
+# tkinter Entry Widget
+from tkinter import *
+
+class MySTATE:
+    def __init__(self, myroot):
+        self.myvar = StringVar()
+        self.myvar.set('Benedict')
+        
+        self.myl1 = Label(myroot, text= 'Normal State')
+        self.myl1.grid(row=0, column=0)
+        
+        self.myl2 = Label(myroot, text='Disabled state')
+        self.myl2.grid(row=1, column=0, pady=10)
+        
+        self.myl3 = Label(myroot, text= 'Readonly state')
+        self.myl3.grid(row=2, column=0, pady=10)
+        
+        self.mye1 = Entry(myroot, textvariable=self.myvar, width=15, state='normal')
+        self.mye1.grid(row=0, column=1, padx=10)
+        
+        self.mye2 = Entry(myroot, textvariable=self.myvar, width=15, state='disabled')
+        self.mye2.grid(row=1, column=1, padx=10)
+        
+        self.mye3 = Entry(myroot, textvariable=self.myvar, width=15, state= 'readonly')
+        self.mye3.grid(row=2, column=1, padx=10)
+        
+if __name__ == '__main__':
+    myroot = Tk()
+    myobj = MySTATE(myroot)
+    myroot.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,20 +1,29 @@
 from tkinter import *
-myroot = Tk()
-myroot.title('Car Selection')
-myroot.geometry('300x300')
 
-myvar = StringVar() # tk variable created
-myvar.set('Cars')
-
-# create an option menu by passing the variable and option list
-myselection = OptionMenu(myroot, myvar, 'BMW', 'Benz', 'Ford', 'Kia', 'Toyota') # variable bound to option menu
-myselection.pack()
-
-# create button with command
-def mydisplay():
-    print('The chosen car :', myvar.get())
-    
-mybtn = Button(myroot, text= 'My Choice',command=mydisplay)
-mybtn.pack(pady=30, side= BOTTOM, anchor= N)
-
-myroot.mainloop()
+class MySTATE:
+    def __init__(self, myroot):
+        self.myvar = StringVar()
+        self.myvar.set('Benedict')
+        
+        self.myl1 = Label(myroot, text= 'Normal State')
+        self.myl1.grid(row=0, column=0)
+        
+        self.myl2 = Label(myroot, text='Disabled state')
+        self.myl2.grid(row=1, column=0, pady=10)
+        
+        self.myl3 = Label(myroot, text= 'Readonly state')
+        self.myl3.grid(row=2, column=0, pady=10)
+        
+        self.mye1 = Entry(myroot, textvariable=self.myvar, width=15, state='normal')
+        self.mye1.grid(row=0, column=1, padx=10)
+        
+        self.mye2 = Entry(myroot, textvariable=self.myvar, width=15, state='disabled')
+        self.mye2.grid(row=1, column=1, padx=10)
+        
+        self.mye3 = Entry(myroot, textvariable=self.myvar, width=15, state= 'readonly')
+        self.mye3.grid(row=2, column=1, padx=10)
+        
+if __name__ == '__main__':
+    myroot = Tk()
+    myobj = MySTATE(myroot)
+    myroot.mainloop()
