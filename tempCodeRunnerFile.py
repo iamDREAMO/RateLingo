c252beb83962deec8355737f07843dc4c89d3e16
iamDREAMO <benedictamofah2@gmail.com>
2025-07-22 13:35:44 +0000

    GUI: Add show/selectborderwidth to Entry()

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,29 +1,29 @@
 from tkinter import *
 
-class MySTATE:
+class MyLogin:
     def __init__(self, myroot):
-        self.myvar = StringVar()
-        self.myvar.set('Benedict')
-        
-        self.myl1 = Label(myroot, text= 'Normal State')
+        self.myl1 = Label(myroot, text= 'Username')
         self.myl1.grid(row=0, column=0)
         
-        self.myl2 = Label(myroot, text='Disabled state')
+        self.myl2 = Label(myroot, text='Password')
         self.myl2.grid(row=1, column=0, pady=10)
         
-        self.myl3 = Label(myroot, text= 'Readonly state')
-        self.myl3.grid(row=2, column=0, pady=10)
+        self.mye1 = Entry(myroot, width=15, selectborderwidth = 3)
+        self.mye1.grid(row=0,column=1,padx=10)
         
-        self.mye1 = Entry(myroot, textvariable=self.myvar, width=15, state='normal')
-        self.mye1.grid(row=0, column=1, padx=10)
-        
-        self.mye2 = Entry(myroot, textvariable=self.myvar, width=15, state='disabled')
+        self.mye2 = Entry(myroot, width=15, show='*')
         self.mye2.grid(row=1, column=1, padx=10)
         
-        self.mye3 = Entry(myroot, textvariable=self.myvar, width=15, state= 'readonly')
-        self.mye3.grid(row=2, column=1, padx=10)
+        def mydisplay():
+            print('The username is: ' + self.mye1.get())
+            print('The password is: ' + self.mye2.get())
+            
+        self.mybtn = Button(myroot, text= 'Login', command=mydisplay, font= ('Cambria', 12, 'bold'))
+        self.mybtn.grid(row=2, columnspan= 3)
         
 if __name__ == '__main__':
     myroot = Tk()
-    myobj = MySTATE(myroot)
+    myobj = MyLogin(myroot)
+    myroot.title('Login Page')
+    myroot.geometry('200x150')
     myroot.mainloop()
