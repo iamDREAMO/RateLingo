6bb5f52a904ad1505d5ba5385083c61242cf5698
iamDREAMO <benedictamofah2@gmail.com>
2025-07-22 09:00:36 +0000

    GUI: Add OptionMenu widget

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -833,3 +833,26 @@ myrb1.pack(padx=10,pady=10)
 myrb2.pack(padx=10,pady=10)
 
 myroot.mainloop()
+
+
+# tkinter OptionMenu Widget
+from tkinter import *
+myroot = Tk()
+myroot.title('Car Selection')
+myroot.geometry('300x300')
+
+myvar = StringVar() # tk variable created
+myvar.set('Cars')
+
+# create an option menu by passing the variable and option list
+myselection = OptionMenu(myroot, myvar, 'BMW', 'Benz', 'Ford', 'Kia', 'Toyota') # variable bound to option menu
+myselection.pack()
+
+# create button with command
+def mydisplay():
+    print('The chosen car :', myvar.get())
+    
+mybtn = Button(myroot, text= 'My Choice',command=mydisplay)
+mybtn.pack(pady=30, side= BOTTOM, anchor= N)
+
+myroot.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,20 @@
 from tkinter import *
 myroot = Tk()
+myroot.title('Car Selection')
+myroot.geometry('300x300')
 
-myrb1 = Radiobutton(myroot, value=0, text='Radiobutton1', bg= 'lightGreen', indicatoron=False)
-myrb2 = Radiobutton(myroot, value=1, text='RadioButton2', bg = 'lightGreen', indicatoron=False)
-myrb1.pack(padx=10,pady=10)
-myrb2.pack(padx=10,pady=10)
+myvar = StringVar() # tk variable created
+myvar.set('Cars')
+
+# create an option menu by passing the variable and option list
+myselection = OptionMenu(myroot, myvar, 'BMW', 'Benz', 'Ford', 'Kia', 'Toyota') # variable bound to option menu
+myselection.pack()
+
+# create button with command
+def mydisplay():
+    print('The chosen car :', myvar.get())
+    
+mybtn = Button(myroot, text= 'My Choice',command=mydisplay)
+mybtn.pack(pady=30, side= BOTTOM, anchor= N)
 
 myroot.mainloop()
