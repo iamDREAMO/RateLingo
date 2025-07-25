dc6a264433ea9b898ed2d5cba4481a098a281fe7
iamDREAMO <benedictamofah2@gmail.com>
2025-07-25 14:57:22 +0000

    GUI: Add Scrollbar to Entry

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -1145,3 +1145,27 @@ if __name__ =='__main__':
     root = Scrollbar_Canvas()
     root.geometry('300x250')
     root.mainloop() 
+    
+
+# Scrollbar attached to Entry
+from tkinter import *
+
+class Scrollbar_Entry(Tk):
+    def __init__(self):
+        super().__init__()
+        self.sclhbar = Scrollbar(self, orient=HORIZONTAL)
+        self.sclhbar.pack(side = BOTTOM, fill=X)
+        
+        self.mye1 = Entry(self, xscrollcommand=self.sclhbar.set) # creation of entry horizontal scrollbars attached
+        self.mye1.pack(expand=0, fill=BOTH)
+        
+        # horizontal elements
+        for loop in range(30): # insertelements from 0 to 29 in the listbox
+            self.mye1.insert(END, str(loop) + '\t')
+            
+            self.sclhbar.config(command=self.mye1.xview)
+            
+if __name__=='__main__':
+    root = Scrollbar_Entry()
+    root.geometry('300x100')
+    root.mainloop()
