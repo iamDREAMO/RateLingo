7813a3e12fdfc85c794fe2daab20b8513301f55b
iamDREAMO <benedictamofah2@gmail.com>
2025-07-25 14:36:23 +0000

    GUI: Add Scrollbar to Text

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -1085,3 +1085,39 @@ class Scrollbar_ListBox(Tk):
 if __name__ == '__main__':
     root = Scrollbar_ListBox()
     root.mainloop()
+
+# Scrollbar attached to Text
+from tkinter import *
+
+class Scrollbar_Text(Tk):
+    def __init__(self):
+        super().__init__()
+        
+        self.mysclbar = Scrollbar(self) # scrollbar creation and attaching to the main window
+        self.mysclbar.pack(side=RIGHT, fill=Y) # scrollbar added to window right side
+        
+        self.sclhbar =Scrollbar(self, orient=HORIZONTAL)
+        self.sclhbar.pack(side=BOTTOM, fill=X)
+        
+        self.mytext = Text(self, width=600, height=600, 
+                           yscrollcommand=self.mysclbar.set,
+                           xscrollcommand=self.sclhbar.set,
+                           wrap=NONE) # creation of textbox ad both horizontal/vertical scrillbars are attached to the textbox
+        self.mytext.pack(expand=0, fill=BOTH)    
+        
+        # horizontal elements
+        for loop in range(30): # insertelements from 0 to 49 in the text
+            self.mytext.insert(END, str(loop) + '\t')
+            
+        # vertical elements
+        for loop in range(50): # insertelements from 0 to 49 in the text
+            self.mytext.insert(END, str(loop) + '\n')
+            
+        self.sclhbar.config(command=self.mytext.xview) # for need of horizontal view settings scrollbar command option to textbox.xview method
+        self.mysclbar.config(command=self.mytext.yview) # for need of horizontal view settings scrollbar command option to textbox.yview method
+        
+if __name__ == '__main__':
+    root = Scrollbar_Text()
+    root.geometry('300x300')
+    root.mainloop()       
+            
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,20 +1,33 @@
 from tkinter import *
-class Scrollbar_ListBox(Tk):
+
+class Scrollbar_Text(Tk):
     def __init__(self):
         super().__init__()
         
-        self.mysclbar = Scrollbar(self) # scrollbar creation and attaching to the menu window
-        self.mysclbar.pack(side=RIGHT, fill='y') # scrollbar added to the window right side 
+        self.mysclbar = Scrollbar(self) # scrollbar creation and attaching to the main window
+        self.mysclbar.pack(side=RIGHT, fill=Y) # scrollbar added to window right side
         
-        self.mylstbox = Listbox(self) # listbox creation and attaching to the main window
-        self.mylstbox.config(yscrollcommand=self.mysclbar.set) # scrollbar attached to the listbox for vertical scroll used yscrollcommand
+        self.sclhbar =Scrollbar(self, orient=HORIZONTAL)
+        self.sclhbar.pack(side=BOTTOM, fill=X)
         
-        for loop in range(50): # insertelements from 0 to 49 in the listbox
-            self.mylstbox.insert(END, str(loop))
+        self.mytext = Text(self, width=600, height=600, 
+                           yscrollcommand=self.mysclbar.set,
+                           xscrollcommand=self.sclhbar.set,
+                           wrap=NONE) # creation of textbox ad both horizontal/vertical scrillbars are attached to the textbox
+        self.mytext.pack(expand=0, fill=BOTH)    
+        
+        # horizontal elements
+        for loop in range(30): # insertelements from 0 to 49 in the text
+            self.mytext.insert(END, str(loop) + '\t')
             
-        self.mylstbox.pack(side='left', fill=BOTH) # listbox added to the window left side
-        self.mysclbar.config(command=self.mylstbox.yview) # for need of vertical view settings scrollbar command option to listbox.yview method
+        # vertical elements
+        for loop in range(50): # insertelements from 0 to 49 in the text
+            self.mytext.insert(END, str(loop) + '\n')
+            
+        self.sclhbar.config(command=self.mytext.xview) # for need of horizontal view settings scrollbar command option to textbox.xview method
+        self.mysclbar.config(command=self.mytext.yview) # for need of horizontal view settings scrollbar command option to textbox.yview method
         
 if __name__ == '__main__':
-    root = Scrollbar_ListBox()
-    root.mainloop()
+    root = Scrollbar_Text()
+    root.geometry('300x300')
+    root.mainloop()    
