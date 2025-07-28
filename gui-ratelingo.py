50ced470fad4f8d6709f95ceb01cc4904c816ded
iamDREAMO <benedictamofah2@gmail.com>
2025-07-28 12:45:24 +0000

    GUI: disabled clicking in spinbox

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -1213,3 +1213,14 @@ spin = Spinbox(font= ('Cambria', 14, 'bold'), command=display,
                         'Magenta', 'Yellow'], textvariable=stv)
 spin.pack()
 root.mainloop()
+
+# disable clicking in spinbox
+from tkinter import * 
+root = Tk()
+root.geometry('350x200')
+root.title('SpinBox with Disabled Clicking')
+
+# spinbox creation
+spin1 = Spinbox(font= ('Cambria', 14, 'bold'), values=(10,35,49,50,60,23), state='readonly')
+spin1.pack(pady=10)
+root.mainloop()
