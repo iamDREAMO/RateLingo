d495172e54db372a2450e4eadbbfbbaa3ac4eda1
iamDREAMO <benedictamofah2@gmail.com>
2025-07-28 13:44:45 +0000

    GUI: Add Text() widget

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -1257,3 +1257,14 @@ root.geometry('300x200+120+120')
 root.mainloop()
     
 
+# tkinter Text Widget
+from tkinter import *
+root = Tk()
+root.geometry()
+root.title('Text Widget')
+
+#creating the text widget
+text1 = Text(root, width=18, height=10, font=('Cambria', 12, 'bold'), wrap =WORD, 
+             padx=10, pady=10, bd=4, selectbackground='Grey', selectforeground='Black')
+text1.pack()
+root.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,28 +1,10 @@
-from tkinter import * 
+from tkinter import *
 root = Tk()
+root.geometry()
+root.title('Text Widget')
 
-# creating a float variable value holder
-var1 = DoubleVar()
-
-# creating horizontal slider
-h_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, 
-                 length= 200, width=10, sliderlength=50, label='myscale',
-                 variable=var1)
-
-# set the scale value to 45
-h_slider.set(45)
-h_slider.pack()
-
-def display():
-    # will get the value
-    print(var1.get())
-    # will return the coordinates corresponding to the given scale value
-    print(h_slider.coords(value=var1.get()))
-    
-# creating a button widget
-btn = Button(root, text='GetValue', command=display, bg='LightBlue')
-btn.pack(pady=10)
-
-root.title('Scale_Widget')
-root.geometry('300x200+120+120')
+#creating the text widget
+text1 = Text(root, width=18, height=10, font=('Cambria', 12, 'bold'), wrap =WORD, 
+             padx=10, pady=10, bd=4, selectbackground='Grey', selectforeground='Black')
+text1.pack()
 root.mainloop()
