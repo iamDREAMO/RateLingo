948abe40df2b7d45314e990d64f106d437d0c56f
iamDREAMO <benedictamofah2@gmail.com>
2025-07-28 12:31:05 +0000

    GUI: add specified values to spinbox

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -1184,3 +1184,13 @@ spin.pack()
 root.mainloop()
 
 
+# specify set of values in spinbox
+from tkinter import *
+root = Tk()
+root.geometry('200x200')
+root.title('SpinBox Values Specified')
+
+# creation of spinbox
+spin1 = Spinbox(font= ('Cambria', 14, 'bold'), values=(10,15,20,25,30), bd=10, relief= RIDGE)
+spin1.pack()
+root.mainloop()
