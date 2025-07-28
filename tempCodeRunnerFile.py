b0da53cfd04e66920781e009f06b5143100a2bcb
iamDREAMO <benedictamofah2@gmail.com>
2025-07-28 12:31:05 +0000

    GUI: add specified values to spinbox

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,10 @@
+# specify set of values in spinbox
 from tkinter import *
-
 root = Tk()
 root.geometry('200x200')
-root.title('SpinBox')
+root.title('SpinBox Values Specified')
 
-# spinbox creation
-spin = Spinbox(font=('Cambria', 14, 'bold'), from_=10, to=20)
-spin.pack()
+# creation of spinbox
+spin1 = Spinbox(font= ('Cambria', 14, 'bold'), values=(10,15,20,25,30), bd=10, relief= RIDGE)
+spin1.pack()
 root.mainloop()
