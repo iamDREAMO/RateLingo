5d851c4f45c226ae1ff609f7f74e864b173c2315
iamDREAMO <benedictamofah2@gmail.com>
2025-07-28 15:20:03 +0000

    GUI: read all content/messagebox added to Text()

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -1268,3 +1268,30 @@ text1 = Text(root, width=18, height=10, font=('Cambria', 12, 'bold'), wrap =WORD
              padx=10, pady=10, bd=4, selectbackground='Grey', selectforeground='Black')
 text1.pack()
 root.mainloop()
+
+# Read entire contents in Text Widget
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.geometry('500x300')
+root.title('Text_Widget Content')
+
+# Text widget creation
+txt = Text(root, width=18, height=12, 
+           font=('Arial', 12), wrap = WORD,
+           padx=10, pady=10, bd=4, 
+           selectbackground='Grey', selectforeground='Blue')
+txt.pack()
+
+# inserting text in the text widget
+txt.insert('1.0', 'Beginner! Welcome to learning tkinter text widget. \n This is for multiple lines.')
+
+# callback function
+def myget():
+    messagebox.showinfo('Text widget contents are: ',
+                        txt.get('1.0', 'end')) # reading the entire content displaying
+    
+btn = Button(root, text='Read', command=myget)
+btn.pack()
+root.mainloop() 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,25 @@
 from tkinter import *
-root = Tk()
-root.geometry()
-root.title('Text Widget')
+from tkinter import messagebox
 
-#creating the text widget
-text1 = Text(root, width=18, height=10, font=('Cambria', 12, 'bold'), wrap =WORD, 
-             padx=10, pady=10, bd=4, selectbackground='Grey', selectforeground='Black')
-text1.pack()
-root.mainloop()
+root = Tk()
+root.geometry('500x300')
+root.title('Text_Widget Content')
+
+# Text widget creation
+txt = Text(root, width=18, height=12, 
+           font=('Arial', 12), wrap = WORD,
+           padx=10, pady=10, bd=4, 
+           selectbackground='Grey', selectforeground='Blue')
+txt.pack()
+
+# inserting text in the text widget
+txt.insert('1.0', 'Beginner! Welcome to learning tkinter text widget. \n This is for multiple lines.')
+
+# callback function
+def myget():
+    messagebox.showinfo('Text widget contents are: ',
+                        txt.get('1.0', 'end')) # reading the entire content displaying
+    
+btn = Button(root, text='Read', command=myget)
+btn.pack()
+root.mainloop() 
