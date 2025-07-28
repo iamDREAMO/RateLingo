1f9c8ce855f8ecea4ab822245755c7353ac051e5
iamDREAMO <benedictamofah2@gmail.com>
2025-07-28 13:09:59 +0000

    GUI: add horizontal slider/scale

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -1224,3 +1224,36 @@ root.title('SpinBox with Disabled Clicking')
 spin1 = Spinbox(font= ('Cambria', 14, 'bold'), values=(10,35,49,50,60,23), state='readonly')
 spin1.pack(pady=10)
 root.mainloop()
+
+
+# tkinter Scale Widget
+from tkinter import * 
+root = Tk()
+
+# creating a float variable value holder
+var1 = DoubleVar()
+
+# creating horizontal slider
+h_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, 
+                 length= 200, width=10, sliderlength=50, label='myscale',
+                 variable=var1)
+
+# set the scale value to 45
+h_slider.set(45)
+h_slider.pack()
+
+def display():
+    # will get the value
+    print(var1.get())
+    # will return the coordinates corresponding to the given scale value
+    print(h_slider.coords(value=var1.get()))
+    
+# creating a button widget
+btn = Button(root, text='GetValue', command=display, bg='LightBlue')
+btn.pack(pady=10)
+
+root.title('Scale_Widget')
+root.geometry('300x200+120+120')
+root.mainloop()
+    
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,28 @@
 from tkinter import * 
 root = Tk()
-root.geometry('350x200')
-root.title('SpinBox with Disabled Clicking')
 
-# spinbox creation
-spin1 = Spinbox(font= ('Cambria', 14, 'bold'), values=(10,35,49,50,60,23), state='readonly')
-spin1.pack(pady=10)
+# creating a float variable value holder
+var1 = DoubleVar()
+
+# creating horizontal slider
+h_slider = Scale(root, from_=0, to=100, orient=HORIZONTAL, 
+                 length= 200, width=10, sliderlength=50, label='myscale',
+                 variable=var1)
+
+# set the scale value to 45
+h_slider.set(45)
+h_slider.pack()
+
+def display():
+    # will get the value
+    print(var1.get())
+    # will return the coordinates corresponding to the given scale value
+    print(h_slider.coords(value=var1.get()))
+    
+# creating a button widget
+btn = Button(root, text='GetValue', command=display, bg='LightBlue')
+btn.pack(pady=10)
+
+root.title('Scale_Widget')
+root.geometry('300x200+120+120')
 root.mainloop()
