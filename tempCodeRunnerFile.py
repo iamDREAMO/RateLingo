cb72e62ac1e5f53f7409bd92b2e9ece9f5e65342
iamDREAMO <benedictamofah2@gmail.com>
2025-07-30 14:43:56 +0000

    GUI: Add image to text widget

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,22 +1,19 @@
 from tkinter import *
-
 root = Tk()
-root.geometry('300x300')
-root.title('Tag to Text_Wigdet')
 
-text = Text(root, width=18, height=10, font=('Cambria', 14, 'bold'), wrap=WORD, padx=10,
-            pady=10, bd=4, selectbackground='Blue', selectforeground='Green')
-text.pack()
+def click():
+    text1.insert('insert',"<>")
+    
+btn = Button(root, text='Click Here!', command=click)
+btn.pack()
 
-text.insert('1.0', 'This is the First line')
-text.insert('1.0 + 1 line', '\nThis is Second Line')
-text.insert('1.0 + 2 lines', '\nThis is the Third Line')
+text1 = Text(root, width=55, height=30)
+text1.pack()
 
-# 1st par: Name of the tag which  will be created as a string
-# 2nd par: start
-# 3rd pas: end
-text.tag_add('Tag', '1.0', '1.0 wordend')
+def insertimage():
+    text1.image_create('insert', image = image1)
 
-# Confir=guring the properties about the tag
-text.tag_configure('Tag', background='Brown')
+image1 = PhotoImage(file='Add-icon.png')
+btn1 = Button(root, text='CreateImage', command=insertimage)
+btn1.pack(pady=10)
 root.mainloop()
