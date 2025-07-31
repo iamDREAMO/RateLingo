956ed60894a42c34a8aacd40d8789827d99879ce
iamDREAMO <benedictamofah2@gmail.com>
2025-07-31 05:15:29 +0000

    GUI: Add String list to combobox

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -1379,3 +1379,20 @@ List1 = list(range(1,10))
 combo = Combobox(root, values=List1, width=15)
 combo.pack(padx=40, pady=10) 
 root.mainloop()
+
+
+# Add string list as values in Combobox
+from tkinter import *
+from tkinter.ttk import Combobox
+
+root = Tk()
+root.geometry('350x200')
+root.title('Combobox with String as Values')
+
+# string values
+str_v = ['Translation', 'Transcription', 'Voice Over', 'Subtitling']
+
+combo1 = Combobox(root, values= str_v, height=2)
+combo1.pack()
+
+root.mainloop()
