ad840990e0f23481121f9b9e2e9cb05ee8332269
iamDREAMO <benedictamofah2@gmail.com>
2025-07-31 05:30:10 +0000

    GUI: Add text display to combobox

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -2,13 +2,19 @@ from tkinter import *
 from tkinter.ttk import Combobox
 
 root = Tk()
-root.geometry('350x200')
-root.title('Combobox with String as Values')
+root.geometry('300x200')
+root.title('Text in Combobox')
 
-# string values
-str_v = ['Translation', 'Transcription', 'Voice Over', 'Subtitling']
+str_val = StringVar()
 
-combo1 = Combobox(root, values= str_v, height=2)
-combo1.pack()
+def display():
+    str_val = combo.get()
+    print(str_val)
+    
+list1 = ['Ghana','Togo','Algeria','Morocco']
+str_val.set('Ghana')
+
+combo = Combobox(root, values= list1, height=5, textvariable=str_val, postcommand=display)
+combo.pack()
 
 root.mainloop()
