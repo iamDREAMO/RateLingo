73bd80b0a153eeb7c8f327ae72467cae69151030
iamDREAMO <benedictamofah2@gmail.com>
2025-07-31 05:03:53 +0000

    GUI: Add Combobox widget

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,19 +1,13 @@
 from tkinter import *
+from tkinter.ttk import Combobox
+
 root = Tk()
+root.geometry('300x200')
+root.title('Combobox Widget')
 
-def click():
-    text1.insert('insert',"<>")
-    
-btn = Button(root, text='Click Here!', command=click)
-btn.pack()
+# List of values
+List1 = list(range(1,10))
 
-text1 = Text(root, width=55, height=30)
-text1.pack()
-
-def insertimage():
-    text1.image_create('insert', image = image1)
-
-image1 = PhotoImage(file='Add-icon.png')
-btn1 = Button(root, text='CreateImage', command=insertimage)
-btn1.pack(pady=10)
+combo = Combobox(root, values=List1, width=15)
+combo.pack(padx=40, pady=10) 
 root.mainloop()
