5eb6cfdefdf0078ff8f7b093c9c882bb07fd6940
iamDREAMO <benedictamofah2@gmail.com>
2025-07-31 05:30:10 +0000

    GUI: Add text display to combobox

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -1396,3 +1396,25 @@ combo1 = Combobox(root, values= str_v, height=2)
 combo1.pack()
 
 root.mainloop()
+
+# Displaying text in the combobox
+from tkinter import *
+from tkinter.ttk import Combobox
+
+root = Tk()
+root.geometry('300x200')
+root.title('Text in Combobox')
+
+str_val = StringVar()
+
+def display():
+    str_val = combo.get()
+    print(str_val)
+    
+list1 = ['Ghana','Togo','Algeria','Morocco']
+str_val.set('Ghana')
+
+combo = Combobox(root, values= list1, height=5, textvariable=str_val, postcommand=display)
+combo.pack()
+
+root.mainloop()
