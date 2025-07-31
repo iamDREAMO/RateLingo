1936f989b122b44bd7a0df760567dbd754c40603
iamDREAMO <benedictamofah2@gmail.com>
2025-07-31 15:29:46 +0000

    GUI: Add Label widget

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -1447,3 +1447,15 @@ combo.current(1)
 root.option_add('*TCombobox*Listbox.font', font1)
 root.mainloop()
 
+
+# Getting Insights of Display Widgets in tkinter
+from tkinter import *
+root = Tk()
+
+root.maxsize(300,300) # maximum size of the window set up to 300 only
+root.resizable(0,0) # fixed window size
+
+label1 = Label(root, text= 'Tkinter\nis\nAwesome', font=('Cambria', 18, 'bold'),
+               bg= 'Cyan', fg= 'Black', width= '16', height= '4')
+label1.pack()
+root.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,25 +1,11 @@
+# Getting Insights of Display Widgets in tkinter
 from tkinter import *
-from tkinter.ttk import Combobox
-
 root = Tk()
-root.geometry('350x300')
-root.title('Specified Font in Combobox') 
 
-# list of values
-list2 = ['Translation', 'Transcription', 'Subtitling','Voice Dubbing']
+root.maxsize(300,300) # maximum size of the window set up to 300 only
+root.resizable(0,0) # fixed window size
 
-# assigning font
-font1 = ('Cambria', 12, 'bold', 'italic')
-
-# assigning label
-lab1 = Label(root, text = 'Choose the service', font=('Cambria', 12, 'bold'))
-lab1.pack(pady=2)
-
-# combobox object
-combo = Combobox(root, values= list2, height=5, font= font1)
-combo.pack(pady= 2)
-combo.current(1)
-
-# specify font of the box of combobox
-root.option_add('*TCombobox*Listbox.font', font1)
+label1 = Label(root, text= 'Tkinter\nis\nAwesome', font=('Cambria', 18, 'bold'),
+               bg= 'Cyan', fg= 'Black', width= '16', height= '4')
+label1.pack()
 root.mainloop()
