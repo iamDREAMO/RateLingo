db438845a9bdcb35a09ca8eef8a226f70feba17d
iamDREAMO <benedictamofah2@gmail.com>
2025-07-31 05:03:53 +0000

    GUI: Add Combobox widget

--- gui-ratelingo.py
+++ gui-ratelingo.py
@@ -1366,4 +1366,16 @@ root.mainloop()
 
 
 # Tkinter Combobox Widget
+from tkinter import *
+from tkinter.ttk import Combobox
 
+root = Tk()
+root.geometry('300x200')
+root.title('Combobox Widget')
+
+# List of values
+List1 = list(range(1,10))
+
+combo = Combobox(root, values=List1, width=15)
+combo.pack(padx=40, pady=10) 
+root.mainloop()
