cedd3f521d290595ac17f54b47b95cfd0a126f0f
iamDREAMO <benedictamofah2@gmail.com>
2025-08-21 14:40:55 +0000

    GUI; add Message widget

--- gui-studies.py
+++ gui-studies.py
@@ -1551,4 +1551,18 @@ if __name__ == '__main__':
     root.mainloop()
     
     
+# Tkinter Message Widget
+from tkinter import *
+root = Tk()
+Str1 = StringVar()
+
+# message widget object
+msg1 = Message(root, textvariable= Str1, relief=SUNKEN, font =('Arial', 12,'bold'),
+               fg = 'Cyan', bg = 'Brown')
+Str1.set('Display of string message')
+msg1.pack()
+root.geometry('200x200')
+root.mainloop()
+    
+    
 
