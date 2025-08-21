760f1115cfe3e75f68d0b7e421f900a620109344
iamDREAMO <benedictamofah2@gmail.com>
2025-08-21 14:23:47 +0000

    GUI; add StringVar and Textvariable to label

--- gui-studies.py
+++ gui-studies.py
@@ -1531,5 +1531,24 @@ if __name__ == '__main__':
     root.geometry('300x200')
     root.mainloop()
     
+
+# Using textvariable options with label widget
+from tkinter import *
+
+class StrVartext(Tk):
+    def __init__(self):
+        super().__init__()
+        self.vall = StringVar()
+        self.title('StringVar/textvariable as Tk label')
+        self.lab1 = Label(self, font =('Cambria', 12, 'bold'),
+                          textvariable = self.vall, relief = 'groove')
+        self.lab1.pack()
+        self.vall.set('Tkinter is great')
+        
+if __name__ == '__main__':
+    root = StrVartext()
+    root.geometry('300x300')
+    root.mainloop()
+    
     
 
