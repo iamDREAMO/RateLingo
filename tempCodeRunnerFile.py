e0fd170f7bc143d3ff7c1e4b63878eacbf789ca5
iamDREAMO <benedictamofah2@gmail.com>
2025-09-08 16:23:30 -0500

    README.md: Updated file

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-root = Tk()
-
-text1 = 'This session is to test my knowledgr so far with tkinter'
-msg1 = Message(root, text=text1, font =('Cambria', 12, 'bold'),
-               fg="Blue", bg='Magenta', relief= RAISED)
-msg1.pack()
-root.geometry('200x200')
-root.mainloop()
