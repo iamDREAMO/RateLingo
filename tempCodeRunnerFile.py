0e78d3f1422918d70fc39cfd20a7493c5d13b3ea
iamDREAMO <benedictamofah2@gmail.com>
2025-10-06 12:33:16 -0400

    GUI: Add MessageBox

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,18 @@
 from tkinter import *
 
 root = Tk()
-root.geometry('200x150')
-root.title('StatusBar_Example')
+root.geometry('300x300')
 
-statusbar = Label(root, text='Example of StatusBar...', 
-                  bd=1, relief=GROOVE, anchor=W) # for text aliginment within label
+frame1=Frame(root, width=150,height=150, bg='Red')
+frame1.grid(row=0, column=0)
 
-statusbar.pack(side=BOTTOM, fill=X) 
+frame2=Frame(root, width=150,height=150, bg='Green')
+frame2.grid(row=1, column=0)
+
+frame3=Frame(root, width=150,height=150, bg='Blue')
+frame3.grid(row=0, column=1)
+
+frame4=Frame(root, width=150,height=150, bg='Cyan')
+frame4.grid(row=1, column=1)
+
 root.mainloop()
