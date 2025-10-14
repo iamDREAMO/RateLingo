5dae95cfb3af35ea81671c153be19418eb81f100
iamDREAMO <benedictamofah2@gmail.com>
2025-10-14 22:56:54 +0000

    GUI: add input to frame

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,18 +1,26 @@
 from tkinter import *
 
 root = Tk()
-root.geometry('300x300')
+root.geometry('300x200')
+root.title('Frame Input')
 
-frame1=Frame(root, width=150,height=150, bg='Red')
-frame1.grid(row=0, column=0)
+frame1=Frame(root)
+l1= Label(frame1, text = 'Name')
+l1.grid(row=0, column=0)
+l2= Label(frame1, text = 'Age')
+l2.grid(row=1, column=0)
+l3= Label(frame1, text = 'Contact')
+l3.grid(row=2, column=0)
 
-frame2=Frame(root, width=150,height=150, bg='Green')
-frame2.grid(row=1, column=0)
+ent1 = Entry(frame1)
+ent1.grid(row=0, column=1)
+ent2 = Entry(frame1)
+ent2.grid(row=1, column=1)
+ent3 = Entry(frame1)
+ent3.grid(row=2, column=1)
 
-frame3=Frame(root, width=150,height=150, bg='Blue')
-frame3.grid(row=0, column=1)
-
-frame4=Frame(root, width=150,height=150, bg='Cyan')
-frame4.grid(row=1, column=1)
+btn = Button(frame1, text= 'View')
+btn.grid(row=3, columnspan= 2)
+frame1.grid(row=3, column=0)
 
 root.mainloop()
